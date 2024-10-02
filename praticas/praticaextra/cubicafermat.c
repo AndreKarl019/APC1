0cfc3009fcd26784bb34b7727764f1709a9590aa
Andre Lopez <andrelopezsollakarl@gmail.com>
2024-10-02 10:52:24 -0300

    Aula06

---
+++ praticas/praticaextra/cubicafermat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int  y, z, k;
+    long int p;
+    float x, n;
+    y = 50;
+    while(p!=z){
+        x = (4*y -3)/12;
+        n = sqrt(x) - 1/2;
+        k = n;
+        z = 3*k^2 + 3*k + 1;
+        p = pow(y,3);
+        y++;
+        printf("%i nao serve ",p);
+        }
+    printf("N = %f e P = %i\n",n,p);
+
+
+    return 0;
+}
---
+++ praticas/praticaextra/testep1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+ int x,y;
+ x=1;
+ y=x++;
+ printf("%i",++x);
+
+    return 0;
+}
